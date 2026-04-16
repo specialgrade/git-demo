59d5af280b38062ad91b446493994adda7f3a30d
Alyssa Marie Dela Cruz <aly.mariedelacruz@gmail.com>
2026-04-16 21:54:16 +0800

    Create BlogController.cs

---
+++ Controllers/BlogController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Assessment5.Controllers
+{
+    public class BlogController : Controller
+    {
+        private static List<Post> posts = new List<Post>
+        {
+            new Post { Id = 1, Title = "First Post", Content = "Ikaw yung nag ShOpLiFfTt", Author = "Alyssa" },
+            new Post { Id = 2, Title = "Second Post", Content = "Six Sevennnn", Author = "Alyssa" }
+        };
+
+        public IActionResult Index()
+        {
+            return View(posts);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var post = posts.FirstOrDefault(p => p.Id == id);
+
+            if (post == null)
+            {
+                return NotFound();
+            }
+
+            return View(post);
+        }
+    }
+}
