8c94e348878c8af826ff404633298402bc154063
Alyssa Marie Dela Cruz <aly.mariedelacruz@gmail.com>
2026-04-16 22:51:12 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,22 +8,19 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseExceptionHandler("/Blog/Error");
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
+
 app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapStaticAssets();
-
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
-
+    pattern: "{controller=Blog}/{action=Index}/{id?}");
 
 app.Run();
